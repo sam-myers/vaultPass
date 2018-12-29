85d230f925bc352aaac5967b1a13a5f1d60e4649
Christopher Blum <zeichenanonym@web.de>
2018-12-29 22:01:49 +0100

    Rewrote much of the code for async / await
    * Removed the chrome API in favor of polyfill
    * Removed jQuery in favor of fetch()
    * Removed bootstrap UI since it depends on jQuery
    * Introduced Material Design Lite instead of bootstrap

--- options.js
+++ options.js
@@ -1,147 +1,161 @@
-/* global authButtonClick chrome jQuery */
-/* export vaultToken */
-/* eslint "space-before-function-paren": ["error", {
-        "anonymous": "always",
-        "named": "never",
-        "asyncArrow": "always"
-    }], */
+/* global authButtonClick browser */
 
-var vaultServerAdress, vaultToken;
-
-function mainLoaded() {
+async function mainLoaded() {
   // get inputs from form elements, server URL, login and password
   var vaultServer = document.getElementById('serverBox');
   var login = document.getElementById('loginBox');
 
-  chrome.storage.sync.get(['vaultAddress'], function (result) {
-    if (result.vaultAddress) {
-      vaultServer.value = result.vaultAddress;
-      vaultServerAdress = result.vaultAddress;
-    }
-  });
-  chrome.storage.sync.get(['username'], function (result) {
-    if (result.username) {
-      login.value = result.username;
-    }
-  });
-  chrome.storage.local.get(['vaultToken'], function (result) {
-    if (result.vaultToken) {
-      vaultToken = result.vaultToken;
-      querySecrets(vaultServerAdress);
-    }
-  });
+  var vaultServerAdress = (await browser.storage.sync.get('vaultAddress')).vaultAddress;
+  if (vaultServerAdress) {
+    vaultServer.value = vaultServerAdress;
+  }
+  var username = (await browser.storage.sync.get('username')).username;
+  if (username) {
+    login.value = username;
+  }
+  var vaultToken = (await browser.storage.local.get('vaultToken')).vaultToken;
+  if (vaultToken) {
+    querySecrets(vaultServerAdress, vaultToken);
+  }
+
   // put listener on login button
   document.getElementById('authButton').addEventListener('click', authButtonClick, false);
   document.getElementById('logoutButton').addEventListener('click', logout, false);
 }
 
-function querySecrets(vaultServerAdress) {
+async function querySecrets(vaultServerAdress, vaultToken) {
   // Hide login prompt if we already have a Token
   document.getElementById('login').style.visibility = 'hidden';
   document.getElementById('logout').style.visibility = 'visible';
   var notify = document.getElementById('notify');
 
-  jQuery.ajax({
-    type: 'LIST',
-    url: vaultServerAdress + '/v1/secret/metadata/vaultPass',
-    headers: { 'X-Vault-Token': vaultToken },
-    contentType: 'application/json',
-    dataType: 'json',
-    success: function (data) {
-      // notify.textContent = 'Available keys ' + JSON.stringify(data.data.keys)
-      displaySecrets(data.data.keys);
+  var fetchListOfSecretDirs = await fetch(`${vaultServerAdress}/v1/secret/metadata/vaultPass`, {
+    method: 'LIST',
+    headers: {
+      'X-Vault-Token': vaultToken,
+      'Content-Type': 'application/json'
     },
-    error: function (data) {
-      notify.textContent = 'ERROR: ' + JSON.stringify(data);
-    }
   });
+  if (!fetchListOfSecretDirs.ok) {
+    notify.textContent = `Fetching list of secret directories failed: ${await fetchListOfSecretDirs.text()}`;
+    throw new Error(`Fetching list of secret directories failed: ${await fetchListOfSecretDirs.text()}`);
+  }
+  displaySecrets((await fetchListOfSecretDirs.json()).data.keys);
 }
 
-function logout() {
+async function logout() {
   document.getElementById('login').style.visibility = 'visible';
   document.getElementById('logout').style.visibility = 'hidden';
   document.getElementById('secretList').innerHTML = '';
   document.getElementById('notify').innerHTML = '';
-
-  chrome.storage.local.set({ 'vaultToken': null }, function () { });
+  await browser.storage.local.set({ 'vaultToken': null });
 }
 
-function displaySecrets(secrets) {
+async function displaySecrets(secrets) {
   var secretList = document.getElementById('secretList');
-  var activeSecrets = [];
   secretList.appendChild(document.createTextNode('Available secret folders'));
   var list = document.createElement('ul');
-  chrome.storage.sync.get(['secrets'], function (result) {
-    activeSecrets = result.secrets;
-    if (!activeSecrets) {
-      activeSecrets = [];
-    }
+  var activeSecrets = (await browser.storage.sync.get('secrets')).secrets;
+  if (!activeSecrets) {
+    activeSecrets = [];
+  }
 
-    secrets.forEach(secret => {
-      // Create the list item:
-      var item = document.createElement('li');
+  for (const secret of secrets) {
+    // Create the list item:
+    var item = document.createElement('li');
 
-      var checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.value = 1;
-      checkbox.name = secret;
-      checkbox.checked = activeSecrets.indexOf(secret) > -1;
-      checkbox.addEventListener('change', secretChanged);
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.value = 1;
+    checkbox.name = secret;
+    checkbox.checked = activeSecrets.indexOf(secret) > -1;
+    checkbox.addEventListener('change', secretChanged);
 
-      item.appendChild(checkbox);
-      // Set its contents:
-      item.appendChild(document.createTextNode('  ' + secret));
+    item.appendChild(checkbox);
+    // Set its contents:
+    item.appendChild(document.createTextNode('  ' + secret));
 
-      // Add it to the list:
-      list.appendChild(item);
-    });
-  });
+    // Add it to the list:
+    list.appendChild(item);
+  }
 
   secretList.appendChild(list);
 }
 
-function secretChanged() {
+async function secretChanged() {
   var checkbox = this;
-  if (this.checked) {
-    chrome.storage.sync.get(['vaultAddress'], function (storage) {
-      jQuery.ajax({
-        type: 'LIST',
-        url: storage.vaultAddress + '/v1/secret/metadata/vaultPass/' + checkbox.name,
-        headers: { 'X-Vault-Token': vaultToken },
-        contentType: 'application/json',
-        dataType: 'json',
-        success: function (data) {
-          chrome.storage.sync.get(['secrets'], function (result) {
-            var secretList = result.secrets;
-            if (!secretList) {
-              secretList = [];
-            }
-            if (secretList.indexOf(checkbox.name > -1)) {
-              secretList.push(checkbox.name);
-            }
-            chrome.storage.sync.set({ 'secrets': secretList }, function () { });
-          });
-        },
-        error: function (data) {
-          console.error('ERROR accessing this field: ' + JSON.stringify(data));
-          checkbox.checked = false;
-          checkbox.disabled = true;
-          checkbox.parentElement.style = 'text-decoration:line-through; color: red;';
-        }
-      });
-    });
-  } else {
-    chrome.storage.sync.get(['secrets'], function (result) {
-      var secretList = result.secrets;
-      if (!secretList) {
-        secretList = [];
-      }
+  var activeSecrets = (await browser.storage.sync.get('secrets')).secrets;
+  if (!activeSecrets) {
+    activeSecrets = [];
+  }
 
-      for (let index = secretList.indexOf(checkbox.name); index > -1; index = secretList.indexOf(checkbox.name)) {
-        secretList.splice(index, 1);
-      }
-      chrome.storage.sync.set({ 'secrets': secretList }, function () { });
+  if (this.checked) {
+    var vaultServerAdress = (await browser.storage.sync.get('vaultAddress')).vaultAddress;
+    var vaultToken = (await browser.storage.local.get('vaultToken')).vaultToken;
+    if (!vaultToken) {
+      throw new Error('secretChanged: Vault Token is empty after login');
+    }
+
+    var fetchListOfSecretsForDir = await fetch(`${vaultServerAdress}/v1/secret/metadata/vaultPass/${checkbox.name}`, {
+      method: 'LIST',
+      headers: {
+        'X-Vault-Token': vaultToken,
+        'Content-Type': 'application/json'
+      },
     });
+    if (!fetchListOfSecretsForDir.ok) {
+      checkbox.checked = false;
+      checkbox.disabled = true;
+      checkbox.parentElement.style = 'text-decoration:line-through; color: red;';
+      throw new Error(`ERROR accessing this field: ${await fetchListOfSecretsForDir.text()}`);
+    }
+    if (activeSecrets.indexOf(checkbox.name > -1)) {
+      activeSecrets.push(checkbox.name);
+    }
+    await browser.storage.sync.set({ 'secrets': activeSecrets });
+
+  } else {
+    for (let index = activeSecrets.indexOf(checkbox.name); index > -1; index = activeSecrets.indexOf(checkbox.name)) {
+      activeSecrets.splice(index, 1);
+    }
+    await browser.storage.sync.set({ 'secrets': activeSecrets });
+  }
+}
+
+// invoked after user clicks "login to vault" button, if all fields filled in, and URL passed regexp check.
+async function authToVault(vaultServer, username, password, authMount) {
+  var loginToVault = await fetch(`${vaultServer}/v1/auth/${authMount}/login/${username}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ 'password': password }),
+  });
+  if (!loginToVault.ok) {
+    new Error(`authToVault: ${await loginToVault.text}`);
+  }
+  const token = (await loginToVault.json()).auth.client_token;
+  await browser.storage.local.set({ 'vaultToken': token });
+  querySecrets(vaultServer, token);
+  // TODO: Use user token to generate app token with 20h validity - then use THAT token
+
+}
+
+async function authButtonClick() {
+  var notify = document.getElementById('notify');
+  // get inputs from form elements, server URL, login and password
+  var vaultServer = document.getElementById('serverBox');
+  var login = document.getElementById('loginBox');
+  var authMount = document.getElementById('authMount');
+  var pass = document.getElementById('passBox');
+  // verify input not empty. TODO: verify correct URL format.
+  if ((vaultServer.value.length > 0) && (login.value.length > 0) && (pass.value.length > 0)) {
+    // if input fields are not empty, attempt authorization to specified vault server URL.
+    await browser.storage.sync.set({ 'vaultAddress': vaultServer.value });
+    await browser.storage.sync.set({ 'username': login.value });
+    authToVault(vaultServer.value, login.value, pass.value, authMount.value);
+  } else {
+    notify.textContent = 'Bad input, must fill in all 3 fields.';
   }
 }
 
--- popup.js
+++ popup.js
@@ -1,70 +1,57 @@
-/* global chrome jQuery */
-/* eslint "space-before-function-paren": ["error", {
-        "anonymous": "always",
-        "named": "never",
-        "asyncArrow": "always"
-    }], */
+/* global browser */
 
-var vaultServerAdress, vaultToken, secretList, currentUrl;
-
-function mainLoaded() {
+async function mainLoaded() {
   var resultList = document.getElementById('resultList');
+  var currentUrl;
 
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    for (let tabIndex = 0; tabIndex < tabs.length; tabIndex++) {
-      var tab = tabs[tabIndex];
-      if (tab.url) {
-        currentUrl = tab.url;
-      }
+  var tabs = await browser.tabs.query({ active: true, currentWindow: true });
+  for (let tabIndex = 0; tabIndex < tabs.length; tabIndex++) {
+    var tab = tabs[tabIndex];
+    if (tab.url) {
+      currentUrl = tab.url;
+      break;
     }
-  });
+  }
 
-  chrome.storage.local.get(['vaultToken'], function (result) {
-    if (!result.vaultToken || result.vaultToken.length === 0) {
-      let message = 'No Vault-Token information available\nPlease use the options page to login';
-      var notify = document.getElementById('notify');
-      notify.innerText = message;
-      notify.style = 'color: red;';
-      console.error(message);
-      return;
-    }
+  var vaultToken = (await browser.storage.local.get('vaultToken')).vaultToken;
+  if (!vaultToken || vaultToken.length === 0) {
+    let message = 'No Vault-Token information available\nPlease use the options page to login';
+    var notify = document.getElementById('notify');
+    notify.innerText = message;
+    notify.style = 'color: red;';
+    return;
+  }
 
-    vaultToken = result.vaultToken;
-    chrome.storage.sync.get(['vaultAddress'], function (result) {
-      vaultServerAdress = result.vaultAddress;
+  var vaultServerAdress = (await browser.storage.sync.get('vaultAddress')).vaultAddress;
 
-      chrome.storage.sync.get(['secrets'], function (result) {
-        secretList = result.secrets;
-        if (!secretList) {
-          secretList = [];
-        }
-        resultList.textContent = '';
+  var secretList = (await browser.storage.sync.get('secrets')).secrets;
+  if (!secretList) {
+    secretList = [];
+  }
+  resultList.textContent = '';
 
-        secretList.forEach(secret => {
-          jQuery.ajax({
-            type: 'LIST',
-            url: vaultServerAdress + '/v1/secret/metadata/vaultPass/' + secret,
-            headers: { 'X-Vault-Token': vaultToken },
-            contentType: 'application/json',
-            dataType: 'json',
-            success: function (data) {
-              data.data.keys.forEach(element => {
-                var pattern = new RegExp(element);
-                var patternMatches = pattern.test(currentUrl);
-                if (patternMatches) {
-                  getCredentials(vaultServerAdress + '/v1/secret/data/vaultPass/' + secret + element).then((credentials) =>
-                    addCredentials(credentials.data.data, element, resultList));
-                }
-              });
-            },
-            error: function (data) {
-              console.error('ERROR accessing ' + secret + ': ' + JSON.stringify(data));
-            }
-          });
-        });
+  var promises = [];
+  for (const secret of secretList) {
+    promises.push((async function () {
+      var secretsInPath = await fetch(`${vaultServerAdress}/v1/secret/metadata/vaultPass/${secret}`, {
+        method: 'LIST',
+        headers: {
+          'X-Vault-Token': vaultToken,
+          'Content-Type': 'application/json'
+        },
       });
-    });
-  });
+      for (const element of (await secretsInPath.json()).data.keys) {
+        var pattern = new RegExp(element);
+        var patternMatches = pattern.test(currentUrl);
+        if (patternMatches) {
+          const urlPath = `${vaultServerAdress}/v1/secret/data/vaultPass/${secret}${element}`;
+          const credentials = await getCredentials(urlPath);
+          addCredentials(credentials.data.data, element, resultList);
+        }
+      }
+    })());
+  }
+  await Promise.all(promises);
 }
 
 function addCredentials(credentials, credentialName, list) {
@@ -77,33 +64,30 @@ function addCredentials(credentials, credentialName, list) {
 }
 
 async function getCredentials(urlPath) {
-  console.debug('Looking for credentials in ' + urlPath);
-  let result;
-
-  try {
-    result = await jQuery.ajax({
-      type: 'GET',
-      url: urlPath,
-      headers: { 'X-Vault-Token': vaultToken },
-      contentType: 'application/json',
-      dataType: 'json'
-    });
-    return result;
-  } catch (error) {
-    console.error(error);
+  const vaultToken = (await browser.storage.local.get('vaultToken')).vaultToken;
+  const result = await fetch(urlPath, {
+    headers: {
+      'X-Vault-Token': vaultToken,
+      'Content-Type': 'application/json'
+    },
+  });
+  if (!result.ok) {
+    throw new Error(`getCredentials: ${await result.text}`);
   }
+  return await result.json();
 }
 
-function fillCredentialsInBrowser(username, password) {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    for (let tabIndex = 0; tabIndex < tabs.length; tabIndex++) {
-      var tab = tabs[tabIndex];
-      if (tab.url) {
-        currentUrl = tab.url;
-        chrome.tabs.sendMessage(tab.id, { message: 'fill_creds', username: username, password: password }, function () { });
-      }
+async function fillCredentialsInBrowser(username, password) {
+  var tabs = await browser.tabs.query({ active: true, currentWindow: true });
+  for (let tabIndex = 0; tabIndex < tabs.length; tabIndex++) {
+    var tab = tabs[tabIndex];
+    if (tab.url) {
+      // tabs.sendMessage(integer tabId, any message, optional object options, optional function responseCallback)
+
+      browser.tabs.sendMessage(tab.id, { message: 'fill_creds', username: username, password: password });
+      break;
     }
-  });
+  }
 }
 
 document.addEventListener('DOMContentLoaded', mainLoaded, false);
